9ad0b4736b9966d7eea1c187bcf33ba1fbff671c
David Dios <daviddios@MacBook-Pro-de-David.local>
2019-03-04 03:14:04 +0100

    Creado menú

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-$('.parallax-window').parallax({imageSrc: '/img/headerbg2.jpg'});
+$('.parallax-window').parallax({
+    imageSrc: '/img/headerbg2.jpg'
+});
+
+$(document).ready(function () {
+    $('a[href*=#]').bind('click', function (e) {
+        //Evito el salto brusco al clickar en el menú:
+        e.preventDefault();
+        //Defino cada menú como una variable:
+        var target = $(this).attr("href");
+        $('html, body').stop().animate({
+            scrollTop: $(target).offset().top
+        }, 600, function () {
+            location.hash = target;
+        });
+
+        return false;
+    });
+});
+
+$(window).scroll(function () {
+    var scrollDistance = $(window).scrollTop();
+
+    // Devuelvo la clase que está activa:
+    $('.seccion').each(function (i) {
+        if ($(this).position().top <= scrollDistance) {
+            $('.navbar a.active').removeClass('active');
+            $('.navbar a').eq(i).addClass('active');
+        }
+    });
+}).scroll();
